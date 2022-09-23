3e190a8d7ae66a20ea1cd5a7fc93e39cdb92f2a8
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-23 10:35:59 +0200

    Create Comments

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import CommentCreate from './CommentCreate';
 
 export default function PostList() {
   const [posts, setPosts] = useState({});
@@ -22,6 +23,7 @@ export default function PostList() {
       >
         <div className='card-body'>
           <h3>{post.title}</h3>
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
