ec2b4a4460d863d0caa45cf99851322efc268465
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-23 12:00:39 +0200

    Emit Comment Creation Events

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,7 +15,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[postId] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const postId = req.params.id;
   const { content } = req.body;
@@ -23,6 +24,19 @@ app.post('/posts/:id/comments', (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[postId] = comments;
+
+  await axios
+    .post('http://localhost:4005/events', {
+      type: 'CommentCreated',
+      data: {
+        id: commentId,
+        content,
+        postId,
+      },
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 
   res.status(201).send(comments);
 });
